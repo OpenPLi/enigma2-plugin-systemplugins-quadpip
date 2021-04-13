7c409fcc1ccc2ff7fe768e86d1480ac8187d397a
persianpros <persianpros@yahoo.com>
2021-04-13 15:00:44 +0430

    PEP8 double aggressive E301 ~ E306

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -2,14 +2,17 @@ from . import _, PLUGIN_NAME, PLUGIN_VERSION
 from Plugins.Plugin import PluginDescriptor
 from qpip import QuadPipScreen, setDecoderMode
 
+
 def main(session, **kwargs):
 	session.open(QuadPipScreen)
+
 
 def autoStart(reason, **kwargs):
 	if reason == 0:
 		setDecoderMode("normal")
 	elif reason == 1:
 		setDecoderMode("normal")
+
 
 def Plugins(**kwargs):
 	list = []
