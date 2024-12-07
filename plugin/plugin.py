a3195e60a48241ceea1dc4e4ead703640f4538ad
Littlesat <littlesat@openpli.org>
2024-12-07 10:08:48 +0100

    Resolve side effect caused by previous commit
    It seems in plugin.py the decoder was set to normal via the old style
    and that causes a crash from the plugin because etDecoderMode could not
    be imported anymore. As this is now done via a 'shadow' config and this
    is already set correctly at a restart by enigma2 this needs not to be
    set to 'normal' via autostart anymore. So remove this autostart code.

--- plugin/plugin.py
+++ plugin/plugin.py
@@ -1,31 +1,16 @@
 from __future__ import print_function
 from . import _, PLUGIN_NAME, PLUGIN_VERSION
 from Plugins.Plugin import PluginDescriptor
-from .qpip import QuadPipScreen, setDecoderMode
+from .qpip import QuadPipScreen
 
 
 def main(session, **kwargs):
 	session.open(QuadPipScreen)
 
 
-def autoStart(reason, **kwargs):
-	if reason == 0:
-		setDecoderMode("normal")
-	elif reason == 1:
-		setDecoderMode("normal")
-
-
 def Plugins(**kwargs):
-	list = []
-	list.append(
+	return [(
 		PluginDescriptor(name=_("Enable Quad PiP"),
 		description=_("Quad Picture in Picture"),
 		where=[PluginDescriptor.WHERE_EXTENSIONSMENU],
-		fnc=main))
-
-	list.append(
-		PluginDescriptor(
-		where=[PluginDescriptor.WHERE_AUTOSTART],
-		fnc=autoStart))
-
-	return list
+		fnc=main))]
